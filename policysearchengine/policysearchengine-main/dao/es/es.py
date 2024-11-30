a0c93539f55c8f0a3a0d1001041f62ab56cfb6e4
wwwasdasdasd <3354603220@qq.com>
2024-11-30 18:50:51 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	policysearchengine/policysearchengine-main/config/config.yaml

--- policysearchengine/policysearchengine-main/dao/es/es.py
+++ policysearchengine/policysearchengine-main/dao/es/es.py
@@ -1,14 +1,13 @@
 from elasticsearch import Elasticsearch
-
 import json
 import requests
 from datetime import datetime
 from typing import List
 import yaml
-import warnings
+# import warnings
 
 # warnings.filterwarnings("ignore")
-
+index = 'policy_search'
 query_fmt = '''
 	{
 	  "query": {
@@ -19,19 +18,19 @@ query_fmt = '''
 	            "should": [
 	              {
 	                "match": {
-	                  "title": "%s"
+	                  "title": {}
 	                }
 	              },
 	              {
 	                "match": {
-	                  "content": "%s"
+	                  "content": {}
 	                }
 	              }
 	            ]
 	          }
 	        },
-	        { "bool": {"must": [%s]} }
-	        %s
+	        { "bool": {"must": [{}]} }
+	        {}
 	      ]
 	    }
 	  }
@@ -39,7 +38,7 @@ query_fmt = '''
 	'''
 ResultFmtDSL = '''
 	{
-	  %s
+	  {}
 	  "sort": [
 	    {
 	      "date": {
@@ -47,9 +46,9 @@ ResultFmtDSL = '''
 	      }
 	    }
 	  ],
-	  "from": %d,
-	  "size": %d,
-	  "highlight": %s
+	  "from": {},
+	  "size": {},
+	  "highlight": {}
 	}
 	'''
 
@@ -71,17 +70,17 @@ fmtScoreQuery = '''
 	{
 	  "query": {
 	    "function_score": {
-	      %s
+	      {}
 	      "functions": [
-	        %s
+	        {}
 	      ],
 	      "score_mode": "multiply",
 	      "boost_mode": "multiply"
 	    }
 	  },
-	  "from": %d,
-	  "size": %d,
-	  "highlight": %s
+	  "from": {},
+	  "size": {},
+	  "highlight": {}
 	}'''
 
 fmtScoreFilter = '''
@@ -89,20 +88,20 @@ fmtScoreFilter = '''
 	  "filter": {
 	    "bool": {
 	      "should": [
-	        { "match": { "title": "%s" }},
-	        { "match": { "content": "%s" }}
+	        { "match": { "title": {} }},
+	        { "match": { "content": {} }}
 	      ]
 	    }
 	  },
-	  "weight": %f
+	  "weight": {}
 	}'''
 
 fmtMustDsl = '''
 	{
 	  "bool": {
 	    "should": [
-	      { "match": { "title": "%s" }},
-	      { "match": { "content": "%s" }}
+	      { "match": { "title": {} }},
+	      { "match": { "content": {} }}
 	    ],
 	    "minimum_should_match": 1
 	  }
@@ -113,47 +112,38 @@ exactFmt = '''
 	  "bool": {
 	    "should": [{
 	      "match_phrase": {
-	        "title": "%s"
+	        "title": {}
 	      }
 	    }, {
 	      "match_phrase": {
-	        "content": "%s"
+	        "content": {}
 	      }
 	    }]
 	  }
 	}'''
 
-
+class SearchInput:
+    def __init__(self, text: str, use_score: bool, score_field: dict):
+        self.text = text  # 搜索内容
+        self.use_score = use_score
+        self.score_field = score_field  # 权重分数
 class ESDocument:
-    def __init__(self, title: str, url: str, date: datetime, content: str,
+    def __init__(self, title: str, labels:str,url: str, date: datetime, content: str,abstract: str,keywords: str,classification: str,
                  department_id: int, sdp_ids: List[int], province_id: int):
         self.title = title
+        self.labels = labels
         self.url = url
         self.date = date.isoformat()  # 将日期转换为 ISO 格式字符串
         self.content = content
+        self.abstract = abstract
+        self.keywords = keywords
+        self.classification = classification
         self.department_id = department_id
         self.sdp_ids = sdp_ids
         self.province_id = province_id
 
 
-# def init_es_client(config_path=None):
-#     """初始化并返回Elasticsearch客户端"""
-#     # 初始化配置解析器
-#     config = configparser.ConfigParser()
-#     # 读取配置文件
-#     config.read(config_path)
-#     # 从配置文件中获取Elasticsearch配置
-#     es_host = config.get("es", "addr")
-#     # es_user = config.get('elasticsearch', 'ES_USER')
-#     # es_password = config.get('elasticsearch', 'ES_PASSWORD')
-#
-#     es = Elasticsearch(
-#         hosts=[es_host],
-#         # basic_auth=(es_user, es_password),
-#         verify_certs=False,
-#         ca_certs='conf/http_ca.crt'
-#     )
-#     return es
+
 def init(config_path:str):
     with open(config_path, 'r',encoding='utf-8') as file:
         config = yaml.safe_load(file)  # 读取 YAML 文件并解析为字典
@@ -171,49 +161,9 @@ def init(config_path:str):
     return es  # 返回 ES 客户端
 
 
-def create_index(es, index_name="test-index"):
-    """创建索引，如果索引已存在则忽略"""
-    if not es.indices.exists(index=index_name):
-        es.indices.create(index=index_name)
-
-
-def define_mapping(es, index_name="test-index"):
-    """为索引定义映射"""
-    mapping = {
-        "mappings": {
-            "properties": {
-                "name": {"type": "text"},
-                "age": {"type": "integer"},
-                "email": {"type": "keyword"}
-            }
-        }
-    }
-    es.indices.create(index=index_name, body=mapping, ignore=400)  # ignore=400忽略索引已存在错误
-
-
-def insert_document(es, index_name="test-index", doc_id=None, document=None):
-    """插入文档到指定索引"""
-    es.index(index=index_name, id=doc_id, document=document)
-
-
-def update_document(es, index_name="test-index", doc_id=None, updated_doc=None):
-    """更新指定ID的文档"""
-    es.update(index=index_name, id=doc_id, body={"doc": updated_doc})
-
-
-def delete_document(es, index_name="test-index", doc_id=None):
-    """删除指定ID的文档"""
-    es.delete(index=index_name, id=doc_id)
-
-
-def search_documents(es, index_name="test-index", query=None):
-    """在指定索引中搜索文档"""
-    return es.search(index=index_name, body=query)
-
-
-def index_doc(date: datetime, department_id: int, province_id: int,
-              title: str, url: str, content: str, sdp_ids: List[int], index: str):
-    doc = ESDocument(title, url, date, content, department_id, sdp_ids, province_id)
+def index_doc(title: str,labels:str, url: str,date: datetime, content: str,department_id: int,
+              abstract: str,keywords: str,classification: str,sdp_ids: List[int],province_id: int, index: str):
+    doc = ESDocument(title, labels,url, date, content,  abstract, keywords, classification,department_id, sdp_ids, province_id)
 
     data = json.dumps(doc.__dict__)  # 将文档对象转换为 JSON 格式
     # index = 'your_index_name'  # 这里替换为你的目标索引名称
@@ -226,3 +176,101 @@ def index_doc(date: datetime, department_id: int, province_id: int,
         return
 
     print(response.json())  # 打印索引响应的内容
+
+
+
+def match_all_doc(es:Elasticsearch,index: str):
+    query = {
+        "query": {
+            "match_all": {}
+        }
+    }
+
+    # 执行搜索
+    search = es.search(index=index, body=query)
+    return search
+
+def sprintf(format_str: str, *args) -> str:
+    """
+    使用给定的格式字符串和参数生成格式化后的字符串。
+
+    :param format_str: 格式字符串，类似于 Python 中的字符串格式化
+    :param args: 可变数量的参数，用于格式化
+    :return: 生成的格式化字符串
+    """
+    return format_str.format(*args)
+
+
+def query_fmt_print(text, exact, small_department_id, province_id):
+    query = ","
+
+    if small_department_id == 0 and province_id == 0:
+        query = ""
+    elif small_department_id != 0 and province_id != 0:
+        query += '{{ "match": {{ "small_department_id": {} }} }},'.format(small_department_id)
+        query += '{{ "match": {{ "province_id": {} }} }}'.format(province_id)
+    elif small_department_id != 0 and province_id == 0:
+        query += '{{ "match": {{ "small_department_id": {} }} }}'.format(small_department_id)
+    elif small_department_id == 0 and province_id != 0:
+        query += '{{ "match": {{ "province_id": {} }} }}'.format(province_id)
+
+    exact_trim = [s for s in exact if s != ""]  # 去除空字符串，得到 exact_trim 列表
+
+    exact_query = ""
+    for i in range(len(exact_trim)):
+        exact_query += sprintf(exactFmt, exact_trim[i])
+        if i < len(exact_trim) - 1:
+            exact_query += ","
+    return sprintf(query_fmt, text, text, exact_query, query)
+
+def SearchDocWithSmallDepartmentID(es:Elasticsearch,searchquery: SearchInput, exact:List[str], smalldepartmentID:int, provinceID:int, ffrom:int, size:int):
+    searchFmt = query_fmt_print(searchquery.text, exact, smalldepartmentID, provinceID)
+
+    if not searchquery.use_score:  # 如果UseScore为false
+        query = sprintf(ResultFmtDSL,
+			searchFmt,
+			ffrom-1,
+			size,
+			highlight)
+    else:
+        query = sprintf(fmtScoreQuery,
+			searchFmt,
+			fmtScoreFilters(searchquery.score_field),
+			ffrom-1,
+			size,
+			highlight)  # 假设fmt_score_query和fmt_score_filters已定义
+
+    print(query)
+    query=json.loads(query)
+    # 发送Elasticsearch搜索请求
+    search_result = es.search(
+        index=index,  # 假设index已定义
+        body=query,  # 将查询转换为JSON格式
+        track_total_hits=True  # 启用跟踪总命中数的选项
+    )
+    if search_result.get('error'):
+        print("Error executing search:", search_result['error'])
+        return None
+
+    # 解析search_result中的JSON数据
+    response_data = json.loads(search_result['body'])  # search_result['body']是一个JSON字符串
+
+    return response_data
+
+def fmtScoreFilters(m):
+    filters = []
+    for word, weight in m.items():
+        filter_string = sprintf(fmtScoreFilter,word, word, weight)  # 假设fmt_score_filter已定义
+        filters.append(filter_string)
+    return ", ".join(filters)  # 将所有过滤器字符串用逗号和空格连接
+
+
+
+
+
+
+
+
+
+# es = init('.../config/config.yaml')
+
--- policysearchengine/policysearchengine-main/main.py
+++ policysearchengine/policysearchengine-main/main.py
@@ -1,10 +1,14 @@
-
-from dao.es.es import init
+from datetime import datetime
+from dao.es.es import init,index_doc
 
 
 # def main():
 #     ES = init('./config/config.yaml')
 if __name__ == '__main__':
     config_path = './config/config.yaml'  # 配置文件的路径
-    es_client = init(config_path)  # 调用 init 函数并获取客户端实例
+    es_client = init(config_path)# 调用 init 函数并获取客户端实例
+    date_instance = datetime.strptime("2024.11.30", "%Y.%m.%d")
+    index_doc( 'policy_smartserver', '自我介绍',
+              'https://blog.csdn.net/weixin_46264660/article/details/130238426',date_instance,"我正帅",
+               7,"帅","帅","帅",[1,2,3,4,5,6,7],7,"test")
 
